2fff899796cf32d34e8b8618da3acd50bbbee25a
Vladimir <t.at.coding@gmail.com>
2015-07-29 22:26:28 +0700

    Defined basic structure for a news channel

---
+++ NewsClient.Tests/Model/NewsChannelTests.cs
@@ -0,0 +1,38 @@
+﻿namespace NewsClient.Tests.Model
+{
+	using System;
+	using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+	using NewsClient.Model;
+
+	[TestClass]
+	public class NewsChannelTests
+	{
+		[TestMethod]
+		public void Constructor_NullName_ExceptionThrown()
+		{
+			// Fixture setup
+			string name = null;
+			var feedUri = new Uri("http://news.com/rss");
+
+			// Exercise system
+			// Verify outcome
+			Assert.ThrowsException<ArgumentNullException>(() => new NewsChannel(name, feedUri));
+
+			// Teardown
+		}
+
+		[TestMethod]
+		public void Constructor_NullFeedUri_ExceptionThrown()
+		{
+			// Fixture setup
+			var name = "News channel";
+			Uri feedUri = null;
+
+			// Exercise system
+			// Verify outcome
+			Assert.ThrowsException<ArgumentNullException>(() => new NewsChannel(name, feedUri));
+
+			// Teardown
+		}
+	}
+}
---
+++ NewsClient/Model/NewsChannel.cs
@@ -0,0 +1,19 @@
+﻿namespace NewsClient.Model
+{
+	using System;
+
+	public class NewsChannel
+	{
+		public NewsChannel(string name, Uri feedUri)
+		{
+			if (name == null) throw new ArgumentNullException("name", "Channel name cannot be null");
+			if (feedUri == null) throw new ArgumentNullException("feedUri", "Channel feed uri cannot be null");
+
+			Name = name;
+			FeedUri = feedUri;
+		}
+
+		public string Name { get; private set; }
+		public Uri FeedUri { get; private set; }
+	}
+}
