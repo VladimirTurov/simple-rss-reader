bb640cf231d23dba377286e3b4e2ced8da1c1ba1
Vladimir <t.at.coding@gmail.com>
2015-07-30 00:49:10 +0700

    Added SyndicationFeed-based IRssParser implementation with sample data tests

---
+++ NewsClient/Infrastructure/SyndicationFeedDecorator.cs
@@ -0,0 +1,47 @@
+﻿namespace NewsClient.Infrastructure
+{
+	using System;
+	using System.Collections.Generic;
+	using Windows.Web.Syndication;
+	using Model;
+
+	public class SyndicationFeedDecorator : IRssParser
+	{
+		public NewsFeed Parse(string rss)
+		{
+			var syndicationFeed = new SyndicationFeed();
+			syndicationFeed.Load(rss);
+			return new NewsFeed(TransformRssFeedToNewsItems(syndicationFeed));
+		}
+
+		private IEnumerable<NewsItem> TransformRssFeedToNewsItems(SyndicationFeed syndicationFeed)
+		{
+			var result = new List<NewsItem>();
+			foreach (var syndicationItem in syndicationFeed.Items)
+			{
+				var title = syndicationItem.Title.Text;
+				var text = syndicationItem.Summary.Text;
+				var publicationDate = syndicationItem.PublishedDate;
+
+				Uri imageSource;
+				TryGetImageSource(syndicationItem, out imageSource);
+
+				var newsItem = new NewsItem(title, text, publicationDate, imageSource);
+				result.Add(newsItem);
+			}
+			return result;
+		}
+
+		private void TryGetImageSource(SyndicationItem syndicationItem, out Uri imageSource)
+		{
+			foreach (var link in syndicationItem.Links)
+			{
+				if (!link.MediaType.Contains("image")) continue;
+				imageSource = link.Uri;
+				return;
+			}
+
+			imageSource = null;
+		}
+	}
+}
