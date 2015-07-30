601498d26d8d7fb2f30ff8178e4102559e2c05a9
Vladimir Turov <turov.me@ya.ru>
2015-07-30 09:49:26 +0600

    Feed page is now used as aggration root

--- NewsApplication/View/Pages/NewsFeedPage.xaml.cs
+++ NewsApplication/View/Pages/NewsFeedPage.xaml.cs
@@ -1,18 +1,30 @@
 ﻿namespace NewsApplication.View.Pages
 {
-	using Windows.UI.Xaml.Navigation;
+   using System;
+   using Windows.UI.Xaml.Navigation;
+   using Infrastructure;
+   using NewsClient.Infrastructure;
+   using NewsClient.Model;
+   using NewsClient.ViewModel;
 
-	public sealed partial class NewsFeedPage
-	{
-		public NewsFeedPage()
-		{
-			InitializeComponent();
+   public sealed partial class NewsFeedPage
+   {
+      public NewsFeedPage()
+      {
+         InitializeComponent();
 
-			NavigationCacheMode = NavigationCacheMode.Required;
-		}
+         NavigationCacheMode = NavigationCacheMode.Required;
+      }
 
-		protected override void OnNavigatedTo(NavigationEventArgs e)
-		{
-		}
-	}
+      protected override async void OnNavigatedTo(NavigationEventArgs e)
+      {
+         if (e.NavigationMode == NavigationMode.New)
+         {
+            var vm = new NewsFeedViewModel(new HttpClientProxy(), new SyndicationFeedDecorator(), new StatusBarProxy());
+            var lentaRu = new NewsChannel("Lenta.ru", new Uri("http://lenta.ru/rss"));
+            var gazetaRu = new NewsChannel("Gazeta.ru", new Uri("http://www.gazeta.ru/export/rss/lenta.xml"));
+            await vm.GetLatestNewsAsync(lentaRu, gazetaRu);
+         }
+      }
+   }
 }
