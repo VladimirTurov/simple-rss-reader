cdf970a4011562b390965a88f5cd7706173b4e2c
Vladimir Turov <turov.me@ya.ru>
2015-07-30 11:00:23 +0600

    Added smooth news cover image appearance on load

--- NewsApplication/View/Controls/NewsDetailsView.xaml.cs
+++ NewsApplication/View/Controls/NewsDetailsView.xaml.cs
@@ -1,10 +1,17 @@
 ﻿namespace NewsApplication.View.Controls
 {
+   using Windows.UI.Xaml;
+
    public sealed partial class NewsDetailsView
    {
       public NewsDetailsView()
       {
          this.InitializeComponent();
+      }
+
+      private void OnImageCoverLoaded(object sender, RoutedEventArgs e)
+      {
+         ImageFadeOutAnimation.Begin();
       }
    }
 }
